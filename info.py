208c7209916074af3b444530bc4c33fb0ca1c3e2
SanjarbekAI <sanjarbekwork@gmail.com>
2024-06-22 16:51:37 +0500

    generic views

---
+++ info.py
@@ -0,0 +1,11 @@
+"""
+authors nomli app oching va uni ichida model yaratong.
+first_name, last_name, email, phone number, age, created_at, updated_at
+
+authors -> post
+authors -> get | hammasii ko'rish
+authors/id -> get | bitta authorli ko'rish
+authors/id -> put | update qilish
+authors/id -> patch | update qilish
+authors/id -> delete | delete qilish
+"""
